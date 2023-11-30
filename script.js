6f01844f61f1f598bf1bc76282fdef32ed0b3f40
Abhishek Anil <141896050+Abhishekanil007@users.noreply.github.com>
2023-11-30 21:17:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,28 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	arr.sort(function(a,b){
+		return a - b;
+	});
+
+	let sum = 0;
+
+	while(arr.length > 1)
+		{
+			let combinedlength = arr[0] + arr[1];
+
+			sum+=combinedlength;
+
+			arr.splice(0, 2, combinedlength);
+
+			arr.sort(function(a,b){
+				return a - b;
+			});
+		}
+
+	return sum;
+	
+	
   
 }
 
